5c499d6f658904f25cfb0ce57ce5203078a97cfb
Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com>
2014-01-25 02:34:19 +0100

    stupid freebsd and it's stupid uin32 inodes -.-
    Docker-DCO-1.1-Signed-off-by: Kim "BKC" Carlbäcker <kim.carlbacker@gmail.com> (github: bkcsoft)

--- utils/fs.go
+++ utils/fs.go
@@ -5,36 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
-	"syscall"
 )
-
-// TreeSize walks a directory tree and returns its total size in bytes.
-func TreeSize(dir string) (size int64, err error) {
-	data := make(map[uint64]bool)
-	err = filepath.Walk(dir, func(d string, fileInfo os.FileInfo, e error) error {
-		// Ignore directory sizes
-		if fileInfo == nil {
-			return nil
-		}
-
-		s := fileInfo.Size()
-		if fileInfo.IsDir() || s == 0 {
-			return nil
-		}
-
-		// Check inode to handle hard links correctly
-		inode := fileInfo.Sys().(*syscall.Stat_t).Ino
-		if _, exists := data[inode]; exists {
-			return nil
-		}
-		data[inode] = false
-
-		size += s
-
-		return nil
-	})
-	return
-}
 
 // FollowSymlink will follow an existing link and scope it to the root
 // path provided.
